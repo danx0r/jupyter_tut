826595a8594a859bcee453cb822de602c813444c
danx0r <dan.miller@eye0.com>
2021-05-17 17:58:58 -0700

    sumpin guud

--- burstext/src/index.ts
+++ burstext/src/index.ts
@@ -19,14 +19,23 @@ const extension: JupyterFrontEndPlugin<void> = {
     // Create a blank content widget inside of a MainAreaWidget
     const content = new Widget();
     const widget = new MainAreaWidget({ content });
-    widget.id = 'apod-jupyterlab';
-    widget.title.label = 'Astronomy Picture';
+    widget.id = 'burst-jupyterlab';
+    widget.title.label = 'Burst';
     widget.title.closable = true;
+    let div = document.createElement('div');
+    div.innerHTML = "Let's get ready to burst!<br/><br/>";
+    let button = document.createElement('button');
+    button.innerHTML = "BURST";
+    button.onclick=function () {alert('do something bursty')};
+    div.appendChild(button);
+    content.node.appendChild(div);
+
+    console.log(widget);
 
     // Add an application command
-    const command: string = 'apod:open';
+    const command: string = 'burst:open';
     app.commands.addCommand(command, {
-      label: 'Random Astronomy Picture',
+      label: 'Burst',
       execute: () => {
         if (!widget.isAttached) {
           // Attach the widget to the main work area if it's not there
