66bfc292cf2ff410280da5e1f91c716cf84e24e6
danx0r <dan.miller@eye0.com>
2021-05-19 18:50:55 -0700

    shift to main widget from iframe example

--- server_ext/src/index.ts
+++ server_ext/src/index.ts
@@ -3,13 +3,12 @@ import {
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { ICommandPalette, IFrame } from '@jupyterlab/apputils';
-
-import { PageConfig } from '@jupyterlab/coreutils';
-
+//import { ICommandPalette, IFrame } from '@jupyterlab/apputils';
+//import { PageConfig } from '@jupyterlab/coreutils';
 import { ILauncher } from '@jupyterlab/launcher';
-
 import { requestAPI } from './handler';
+import { ICommandPalette, MainAreaWidget } from '@jupyterlab/apputils';
+import { Widget } from '@lumino/widgets';
 
 /**
  * The command IDs used by the server extension plugin.
@@ -64,7 +63,18 @@ const extension: JupyterFrontEndPlugin<void> = {
       label: 'Get Server Content in a IFrame Widget',
       caption: 'Get Server Content in a IFrame Widget',
       execute: () => {
-        const widget = new IFrameWidget();
+        const content = new Widget();
+        const widget = new MainAreaWidget({ content });
+        widget.id = 'burst-jupyterlab';
+        widget.title.label = 'Burst';
+        widget.title.closable = true;
+        let div = document.createElement('div');
+        div.innerHTML = "Let's get ready to burst!<br/><br/>";
+        let button = document.createElement('button');
+        button.innerHTML = "BURST";
+        button.onclick=function () {alert('do something bursty')};
+        div.appendChild(button);
+        content.node.appendChild(div);
         shell.add(widget, 'main');
       }
     });
@@ -83,14 +93,14 @@ const extension: JupyterFrontEndPlugin<void> = {
 
 export default extension;
 
-class IFrameWidget extends IFrame {
-  constructor() {
-    super();
-    const baseUrl = PageConfig.getBaseUrl();
-    this.url = baseUrl + 'jlab-ext-example/public/index.html';
-    this.id = 'doc-example';
-    this.title.label = 'Server Doc';
-    this.title.closable = true;
-    this.node.style.overflowY = 'auto';
-  }
-}
+//class IFrameWidget extends IFrame {
+//  constructor() {
+//    super();
+//    const baseUrl = PageConfig.getBaseUrl();
+//    this.url = baseUrl + 'jlab-ext-example/public/index.html';
+//    this.id = 'doc-example';
+//    this.title.label = 'Server Doc';
+//    this.title.closable = true;
+//    this.node.style.overflowY = 'auto';
+//  }
+//}
